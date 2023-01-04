bd0062be5eba1c075bb163e239078a924f559b2a
Muhamad-Farhannudin <110012265+Muhamad-Farhannudin@users.noreply.github.com>
2023-01-04 07:01:08 +0700

    Menyelesaikan bagian Navbar dan Header

---
+++ src/Requests.js
@@ -0,0 +1,11 @@
+const key = '932aba656bcdc3891d0c2c0f8e41883d';
+
+const requests = {
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+    requestTopAnime: `https://api.themoviedb.org/3/movie/372058?api_key=${key}&language=en-US`,
+    requestUpComming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+};
+
+export default requests
