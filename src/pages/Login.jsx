6ec95f057818beee464cf5f30eeac30446cab6b9
Muhamad-Farhannudin <110012265+Muhamad-Farhannudin@users.noreply.github.com>
2023-01-10 01:12:40 +0700

    menyelesaikan bagian firebase, dan menyimpan data movie yang disimpan oleh pengguna yang telah Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,16 +5,19 @@ import { UserAuth } from '../context/AuthContext'
 export const Login = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-    const {user, signUp} = UserAuth()
+    const {user, logIn} = UserAuth()
+    const [error, setError] = useState('')
     const navigate = useNavigate()
   
     const handleSubmit = async (e) => {
       e.preventDefault()
+      setError('')
       try {
-        await signUp(email, password)
+        await logIn(email, password);
         navigate('/')
       } catch (error) {
         console.log(error)
+        setError(error.message)
       }
     }
 
@@ -27,9 +30,10 @@ export const Login = () => {
           <div className="max-w-[450px] h-[600px] mx-auto bg-black/75 text-white">
             <div className="max-w-[320px] mx-auto py-16">
               <h1 className="text-3xl font-bold">Sign In</h1>
-              <form className="w-full flex flex-col py-4">
-                <input className="p-3 my-2 bg-gray-700 rounded" type="email" placeholder="Email" autoComplete="email"/>
-                <input className="p-3 my-2 bg-gray-700 rounded" type="password" placeholder="Password" autoComplete="current-password"/>
+              {error ? <p className='p-3 bg-red-400 my-2'>{error}</p> : null}
+              <form onSubmit={handleSubmit} className="w-full flex flex-col py-4">
+                <input onChange={(e) => setEmail(e.target.value)} className="p-3 my-2 bg-gray-700 rounded" type="email" placeholder="Email" autoComplete="email"/>
+                <input onChange={(e) => setPassword(e.target.value)} className="p-3 my-2 bg-gray-700 rounded" type="password" placeholder="Password" autoComplete="current-password"/>
 
                 <button className="bg-red-600 py-3 my-6 rounded font-bold">Sign In</button>
 
